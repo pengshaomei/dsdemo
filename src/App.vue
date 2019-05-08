4fc48e2b8009f6fbb57e5780e620b7731defb624
pengshaomei <18358352865@163.com>
2019-05-08 10:22:47 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h2>我是一个小标题</h2>
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
